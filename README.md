78ef8c4e465e65d00f672f9a6739ac7a81c07800
Filipe Andrade <filipe.keyboardwf@gmail.com>
2021-08-28 10:09:25 -0300

    Adds metarepo

---
+++ README.md
@@ -0,0 +1,7 @@
+# dotfiles
+
+# Setup metarepo
+This repo uses a git bare repo strategy to be sync up and versionable, to setup meta-repo uses 
+```
+command
+```
---
+++ metarepo.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+META_REPO_DIR=$HOME/.cfg
+
+function config {
+	/usr/bin/git --git-dir=$META_REPO_DIR --work-tree=$HOME $@
+}
+
+if [ ! -d $META_REPO_DIR ]; then
+	git clone --bare git@github.com:filipewvandrade/dotfiles.git $HOME/.cfg
+fi
+config config --local status.showUntrackedFiles no
+config checkout
+config status
+
